d4953bd87ac5cecd381d51e5e9ce6478f687efc2
Flaviano Dias (@flavindias) <flavindias@msn.com>
2017-11-12 18:16:11 -0300

    fase 2

---
+++ views/index.jsx
@@ -0,0 +1,30 @@
+// This code uses the optional React.js JSX syntax to create our views, which we
+// shall use throughout the rest of this workshop.
+import React from 'react';
+
+    export default class TodoBox extends React.Component{
+      render() {
+        return (<div className="todoBox">
+            <h1>Todos</h1>
+            <TodoList />
+            <TodoForm />
+          </div>)
+      }
+    }
+    class TodoList extends React.Component{
+      // Escreva o código aqui
+      render(){
+        return (<div class="todoList">
+            I am a TodoList.
+          </div>)
+
+      }
+    }
+    class TodoForm extends React.Component {
+      // Escreva o código aqui
+      render(){
+        return (<div class="todoForm">
+          I am a TodoForm.
+        </div>)
+      }
+    }
