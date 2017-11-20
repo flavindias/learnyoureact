5a8b08bff4a4161ca563724d2ae18ab2a1f7f43a
Flaviano Dias (@flavindias) <flavindias@msn.com>
2017-11-20 16:43:08 -0300

    state solution

--- views/index.jsx
+++ views/index.jsx
@@ -30,15 +30,26 @@ import PropTypes from 'prop-types';
       }
     }
     class Todo extends React.Component{
+      constructor(props) {
+        super(props);
+        this.state = {
+          checked: false
+        };
+      }
+      handleChange(e) {
+        this.setState({
+          checked: e.target.checked
+        });
+      }
+
       render(){
         return(
           <tr>
             <td style={{border: "1px solid black;"}}>
-              {this.props.title}
+              <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
             </td>
-            <td style={{border: "1px solid black;"}}>
-              {this.props.children}
-            </td>
+            <td style={{border: "1px solid black;"}}>{this.props.title}</td>
+            <td style={{border: "1px solid black;"}}>{this.props.children}</td>
           </tr>
         );
       }
